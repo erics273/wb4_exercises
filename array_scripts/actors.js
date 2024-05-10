aed31b9c665236a3e884ce9ea6cefaeb94c5ec59
Eric Schwartz <erics273@gmail.com>
2024-05-10 11:59:26 -0400

    filtering actors and starting sorting

--- array_scripts/actors.js
+++ array_scripts/actors.js
@@ -28,8 +28,10 @@ let academyMembers = [
     }
 ];
 
+//get the length of the list so we know how many times to loop
 let numberOfAcademyMembers = academyMembers.length;
 
+//looping over our academyMembersArray
 for(let i=0; i < numberOfAcademyMembers; i++){
 
     //look for the member with the memID of 187 and display their name
@@ -43,9 +45,27 @@ for(let i=0; i < numberOfAcademyMembers; i++){
     }
 
 
-    //how many members have been in at least 3 films
-    if( academyMembers[i].name.indexOf("Bob") ===  0){
+    //how many have a name that starts with Bob
+    if( academyMembers[i].name.indexOf("Bob") === 0){
         console.log(`${academyMembers[i].name} is in the my name starts with Bob club`);
+    }
+
+    //lets put the actors film in its own variabl
+    let actorsFilms = academyMembers[i].films
+    
+
+    let startsWithA = false;
+    for(let i=0; i < actorsFilms.length; i++){
+
+        if(actorsFilms[i].indexOf("A") === 0){
+            startsWithA = true;
+            break;
+        }
+
+    }
+
+    if(startsWithA){
+        console.log(`${academyMembers[i].name} has been in movies that start with A`)
     }
 
 }
